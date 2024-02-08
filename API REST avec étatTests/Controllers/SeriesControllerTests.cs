2c8b885c1d618c5bef24ac31b487e1c5b399c74e
SIMON GOY <sg.bonneville@gmail.com>
2024-02-08 11:11:12 +0100

    t

--- API REST avec étatTests/Controllers/SeriesControllerTests.cs
+++ API REST avec étatTests/Controllers/SeriesControllerTests.cs
@@ -17,13 +17,7 @@ namespace API_REST_avec_état.Controllers.Tests
     public class SeriesControllerTests
     {
         public SeriesController controller;
-        [TestInitialize]
-        public void Init()
-        {
-            var builder = new DbContextOptionsBuilder<SeriesDBContext>().UseNpgsql("Server=51.83.36.122;port=5432;Database=goysim; SearchPath=schema_api; uid=goysim; password=PjQgs7"); // Chaine de connexion à mettre dans les ( )
-            SeriesDBContext context = new SeriesDBContext(builder.Options);
-            controller = new SeriesController(context);
-        }
+
         
     }
 }
