5c76e70f0e12415a464fe5d454387f9e9b0f49bc
SIMON GOY <sg.bonneville@gmail.com>
2024-02-08 09:32:59 +0100

    Use swagger

--- API REST avec état/Program.cs
+++ API REST avec état/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddDbContext<SeriesDBContext>(options => options.UseNpgsql(buil
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
